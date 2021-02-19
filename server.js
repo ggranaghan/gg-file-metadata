6a7d133b764c142ed750921a2305c1963c2a0f9d
ggranaghan <56278351+ggranaghan@users.noreply.github.com>
2021-02-19 14:30:41 -0500

    initial

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const formidable = require('formidable')
+const bodyParser = require('body-parser')
 var app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
+
+
+app.post('/api/fileanalyse', (req, res) => {
+  new formidable.IncomingForm().parse(req, (err, fields, files) => {
+    if (err) console.error('Error', err)
+    res.send({
+      name: files.upfile.name,
+      type: files.upfile.type,
+      size: files.upfile.size
+    })
+  })
+})
 
 
 
